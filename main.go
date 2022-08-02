d0d3401fa83a7eb7f4d8ad1b51d22f636fd08829
isaackielma <isaac@mysterium.network>
2022-08-02 19:43:18 -0300

    2nd commit

--- main.go
+++ main.go
@@ -1,38 +1,54 @@
 package main
 
-import(
+import (
 	"context"
 	"fmt"
 	"log"
 	"os"
-	
+	"strconv"
+
 	"github.com/shomali11/slacker"
 )
 
-func printCommandEvents(analyticsChannel <-chan*slacker.CommandEvent){
-	for event := range analyticsChannel{
-		fmt.Println("Command Events")
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Evenets")
 		fmt.Println(event.Timestamp)
-		fmt.Println(event.command)
+		fmt.Println(event.Command)
 		fmt.Println(event.Parameters)
 		fmt.Println(event.Event)
 		fmt.Println()
 	}
 }
 
-func main(){
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3637593783267-3872697546516-P0uGLiXjoFq8cX8ZJDzVDjtj")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03R58CT1K9-3869788535091-31234e694cd5f23e1575c29650ab78a4f2a2877b4583f18ebea7964d0df3a1ac")
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-310246501329-2253154075362-5bewxhgvdaoCpRKivBSeG4P4")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A027MA572CC-2246395933878-354ffc9a0aad7282fca446b1999909f75a0b8d48e5284fed1d06e59e18614118")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
 	go printCommandEvents(bot.CommandEvents())
 
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
+		Description: "yob calculator",
+		Example:     "my yob is 2020",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				println("error")
+			}
+			age := 2021 - yob
+			r := fmt.Sprintf("age is %d", age)
+			response.Reply(r)
+		},
+	})
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	err := bot.Listen(ctx)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 }
